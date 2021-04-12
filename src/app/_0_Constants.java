5a9093ab2490fc6ff1010daa64f489a322daf93f
Pi77a <b3narndt@gmail.com>
2021-04-12 16:18:58 +0200

    gsd

--- src/app/_0_Constants.java
+++ src/app/_0_Constants.java
@@ -5,9 +5,9 @@ public class _0_Constants {
 	public static int WINDOW_WIDTH = 800;
 	public static int WINDOW_HEIGHT = 600;
 	
-	public static int FPS = 60;
+	public static int FPS = 70;
 	public static int TPF = 1000/FPS;
 	
-	public static double TIMESCALE = 1.0;
+	public static double TIMESCALE = 6.0;
 	
 }
